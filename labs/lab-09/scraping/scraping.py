b4b02da547c141f6811f32cd635cb4089fb42f0a
clcl915 <cl4750@nyu.edu>
2022-04-28 00:10:04 +0800

    scraping pokemon data

--- labs/lab-09/scraping/scraping.py
+++ labs/lab-09/scraping/scraping.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import urllib.request
+import shutil
+import os
 from bs4 import BeautifulSoup
 from pprint import pprint
 print("works! from vsc")
@@ -9,7 +12,8 @@ url = "https://www.serebii.net/pokemon/gen1pokemon.shtml"
 url_start = "https://www.serebii.net/pokemon/gen"
 url_end = "pokemon.shtml"
 data =[]
-for i in range(1,9):
+os.chdir('images')
+for i in range(8,9): 
     print(i)
     url = url_start + str(i) + url_end
     result = requests.get(url)
@@ -31,42 +35,52 @@ for i in range(1,9):
     # table_of_interest = soup.select(".dextable tr td:nth-child(3)")
     table_of_interest = soup.select_one(".dextable")
     rows = table_of_interest.select("tr")
+
+
     # print(rows[0])
     for row in rows[2::2]:
         cells = row.select("td")
-        
-        pokemonType =[]
-        pokemonTypes= cells[4].select("a")
-        for typetag in pokemonTypes:
-            pokemonType.append(typetag['href'].split("/")[-1])
+        # GET IMG SRC 
+        # print("Current working directory: {0}".format(os.getcwd()))
+        image_url = "https://www.serebii.net"  + cells[1].select_one("img")['src']
+        with open(cells[0].text.strip() + '.png','wb') as f:
+            response = requests.get(image_url, stream=True) 
+            f.write(response.content)
+        # print(image_url,response.status_code)
 
-        pokemonAbility=[]
-        pokemonAbilities= cells[5].select("a")
-        # pprint(pokemonAbilities)
-        for ability in pokemonAbilities:
-            pokemonAbility.append(ability.text)
-        
-        data.append({
-            "number" : cells[0].text.strip(),
-            "imgSrc" : "https://www.serebii.net" + cells[1].select_one("a")['href'] + cells[1].select_one("img")['src'],
-            "title" : cells[3].select_one("a").text,
-            "type": pokemonType,
-            "ability": pokemonAbility,
-            "stats": [ {
-                "hp" : cells[6].text.strip(),
-                "att" : cells[7].text.strip(),
-                "def" : cells[8].text.strip(),
-                "s.att" : cells[9].text.strip(),
-                "s.def" : cells[10].text.strip(),
-                "sp" : cells[11].text.strip()
+        # # GET TYPE 
+        # pokemonType =[]
+        # pokemonTypes= cells[4].select("a")
+        # for typetag in pokemonTypes:
+        #     pokemonType.append(typetag['href'].split("/")[-1])
 
-            }]
-        })
+        # # GET ABILITIES 
+        # pokemonAbility=[]
+        # pokemonAbilities= cells[5].select("a")
+        # for ability in pokemonAbilities:
+        #     pokemonAbility.append(ability.text)
+        
+        # data.append({
+        #     "number" : cells[0].text.strip(),
+        #     "imgSrc" : "https://www.serebii.net" + cells[1].select_one("img")['src'],
+        #     "title" : cells[3].select_one("a").text,
+        #     "type": pokemonType,
+        #     "ability": pokemonAbility,
+        #     "stats": [ {
+        #         "hp" : cells[6].text.strip(),
+        #         "att" : cells[7].text.strip(),
+        #         "def" : cells[8].text.strip(),
+        #         "s.att" : cells[9].text.strip(),
+        #         "s.def" : cells[10].text.strip(),
+        #         "sp" : cells[11].text.strip()
+
+        #     }]
+        # })
         # print("-"*50)
     # print(data)
     # pprint(data)  
     #4. save the dictionary as a data.json file. DONE
-    with open("data.json", "w") as outfile:
-        json.dump(data,outfile,indent=4)
+    # with open("data.json", "w") as outfile:
+    #     json.dump(data,outfile,indent=4)
 
 
